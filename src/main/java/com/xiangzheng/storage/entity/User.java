d15f664f1ae247849f2151d96ae5468122687830
gaohaoyou <1249711374@qq.com>
2020-06-03 21:16:44 +0800

    SAAS 平台动态数据库连接

---
+++ src/main/java/com/xiangzheng/storage/entity/User.java
@@ -0,0 +1,34 @@
+package com.xiangzheng.storage.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created with IDEA
+ *
+ * @author : zhoupan
+ * @date : 2019/4/9 11:40
+ * @info :
+ */
+@Getter
+@Setter
+public class User {
+    String userName;
+    String age;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
